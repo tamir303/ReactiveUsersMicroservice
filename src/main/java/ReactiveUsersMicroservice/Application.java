47d670d0e0268c976e097bf64ed3e162aa13136c
tamir303 <tamir.spilberg@s.afeka.ac.il>
2024-01-07 20:14:14 +0200

    initial commit

---
+++ src/main/java/ReactiveUsersMicroservice/Application.java
@@ -0,0 +1,12 @@
+package ReactiveUsersMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveUsersMicroservice.Application.class, args);
+    }
+}
